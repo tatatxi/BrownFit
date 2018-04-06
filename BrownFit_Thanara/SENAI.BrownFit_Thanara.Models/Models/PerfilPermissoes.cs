f4fb9470def7d3980e875f70e5ea28229be129f3
tatatxi <tatatxi@gmail.com>
2018-04-05 21:55:37 -0300

    permissoes
    permissoes

---
+++ BrownFit_Thanara/SENAI.BrownFit_Thanara.Models/Models/PerfilPermissoes.cs
@@ -0,0 +1,21 @@
+﻿using SENAI.BrownFit_Thanara.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENAI.BrownFit_Thanara.Models.Models
+{
+    public class PerfilPermissoes
+    {
+        public PerfilPermissoes()
+        {
+            PerfilPermissoesId = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid PerfilPermissoesId { get; set; }
+    }
+}
