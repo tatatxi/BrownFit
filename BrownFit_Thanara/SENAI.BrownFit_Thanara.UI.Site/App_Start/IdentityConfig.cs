9fd7522e0b7dae465546dc9b9a38352d1008529a
Thanara Corrêa <tatatxi@gmail.com>
2018-06-14 22:27:35 -0300

    tentativa de roles

---
+++ BrownFit_Thanara/SENAI.BrownFit_Thanara.UI.Site/App_Start/IdentityConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SENAI.BrownFit_Thanara.UI.Site.App_Start
+{
+    public class IdentityConfig
+    {
+        //public class ApplicationRoleManager : RoleManager<IdentityRole, string>
+        //{
+        //    public ApplicationRoleManager(IRoleStore<IdentityRole, string> roleStore)
+        //        : base(roleStore)
+        //    {
+        //    }
+
+        //    public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options, Brown_ThanaraContext context)
+        //    {
+        //        return new ApplicationRoleManager(new RoleStore<IdentityRole>(new ApplicationDbContext()));
+        //    }
+        }
+    }
